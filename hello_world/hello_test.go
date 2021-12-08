c46cbbda5d4b16fa8cde20e2cb9a6a2275eb369e
theoboldalex <theoboldalex@gmail.com>
2021-12-08 21:58:08 +0000

    working test

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello()
-	expected := "Hello World!"
+	result := Hello("Alex")
+	expected := "Hello Alex!"
 
 	if result != expected {
 		t.Errorf("Expected %q but got %q", expected, result)
