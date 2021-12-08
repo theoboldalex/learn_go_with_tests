639aedc761aab2118f92f956f42a70e37dfd97b8
theoboldalex <theoboldalex@gmail.com>
2021-12-08 22:00:26 +0000

    refactor to use constant

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	result := Hello("Alex")
-	expected := "Hello Alex!"
+	expected := "Hello Alex"
 
 	if result != expected {
 		t.Errorf("Expected %q but got %q", expected, result)
