5abf296c938128e5ab5206a68c9db5c6f7b413fe
theoboldalex <theoboldalex@gmail.com>
2021-12-08 22:26:23 +0000

    refactor tests

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Alex")
-	expected := "Hello Alex"
-
-	if result != expected {
-		t.Errorf("Expected %q but got %q", expected, result)
+	assertCorrectMessage := func(t testing.TB, expected, result string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("Expected %q but got %q", expected, result)
+		} 	
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		result := Hello("Alex")
+		expected := "Hello Alex"
+
+		assertCorrectMessage(t, expected, result)
+	})
+
+	t.Run("saying hello world where no name is passed in", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello World"
+
+		assertCorrectMessage(t, expected, result)
+	})
 }
