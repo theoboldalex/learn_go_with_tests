7e9d6b302ef48f8ca838187f921207413366405b
theoboldalex <theoboldalex@gmail.com>
2021-12-09 10:20:41 +0000

    completed iteration section

---
+++ iteration/iteration.go
@@ -0,0 +1,7 @@
+package iteration
+
+import "strings"
+
+func Repeat(character string, times int) (repeated string) {
+	return strings.Repeat(character, times)
+}
---
+++ iteration/iteration_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("Expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("a", 5)
+	fmt.Println(result)
+	// Output: aaaaa
+}
