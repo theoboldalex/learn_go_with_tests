f5bb0392fc4ffe9df7ca50c384d7bfa88dec8d09
theoboldalex <theoboldalex@gmail.com>
2021-12-10 21:09:19 +0000

    arrays and s.ices section completed

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) (sum int) {
 
 	return
 }
+
+func SumAll(numbers ...[]int) (sum []int) {
+	for _, num := range numbers {
+		sum = append(sum, Sum(num))
+	}
+
+	return
+}
+
+func SumAllTails(numbers ...[]int) (sum []int) {
+	for _, num := range numbers {
+		if len(num) == 0 {
+			sum = append(sum, 0)
+			continue
+		}
+		sum = append(sum, Sum(num[1:]))
+	}
+
+	return
+}
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("sum a collection of any length", func(t *testing.T) {
@@ -12,5 +15,37 @@ func TestSum(t *testing.T) {
 		if result != expected {
 			t.Errorf("Expected '%d' but got '%d', Given '%v'", expected, result, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	result := SumAll([]int{1, 2}, []int{3, 4})
+	expected := []int{3, 7}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected '%v' but got '%v'.", expected, result)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, result, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Expected '%v' but got '%v'.", expected, result)
+		}
+
+	}
+	t.Run("sum the tails of some slices", func(t *testing.T) {
+		result := SumAllTails([]int{1, 2, 3}, []int{2, 3, 4, 5})
+		expected := []int{5, 12}
+
+		checkSums(t, result, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		result := SumAllTails([]int{}, []int{2, 3, 4, 5})
+		expected := []int{0, 12}
+
+		checkSums(t, result, expected)
 	})
 }
