70e1f511afca8b0da2e4b87e56549b85d2214ceb
theoboldalex <theoboldalex@gmail.com>
2021-12-17 14:56:18 +0000

    add method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -36,3 +36,28 @@ func assertError(t testing.TB, got, want error) {
 		t.Errorf("got error %q want %q", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t testing.TB, d Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := d.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
+	}
+}
